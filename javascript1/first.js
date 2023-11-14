a478921a3ea4c9eda56f116459a74c59cd2118a1
abhijit singha <112739356+Abhijitsingha-code@users.noreply.github.com>
2023-11-14 16:12:31 +0000

    scope

--- javascript1/first.js
+++ javascript1/first.js
@@ -1,2 +1,28 @@
-console.log(1+2+'3'); //33
-console.log("3"+1+2); //312
+// console.log(1 + 2 + '3'); //33
+// console.log("3" + 1 + 2); //312
+
+const obj1 = [
+    {
+        time: 10,
+    }
+]
+
+const obj2 = [
+    {
+        age: 20,
+    },
+]
+
+// console.log(obj1, obj2)
+
+if(true){
+    variable=10;  //it is by default declare as var type of variable ,so it can be access outside of block scope , var is functional ,global scope
+  }
+
+function x(){
+    a='first';
+}
+
+console.log(variable)
+// console.log(a)  // reference error :a is not defined ,cuz a var are of funtional scope so it can't be access out of the function.
+
